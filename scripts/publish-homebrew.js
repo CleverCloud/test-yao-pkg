aaf8976fb60c30f7ce951d0b0cd94bc97f6a50d8
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2025-07-20 20:37:54 +0200

    chore: wip commit, do not merge

---
+++ scripts/publish-homebrew.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import { getAssetPath } from './lib/paths.js';
+import { getSha256, highlight } from './lib/utils.js';
+import { simpleGit } from 'simple-git';
+import { applyTemplates } from './lib/templates.js';
+import pkg from '../package.json' with { type: 'json' };
+import { commitAndPush } from './lib/git.js';
+
+const GIT_PROJECT = 'homebrew-tap';
+const TEMPLATES_PATH = './scripts/templates/homebrew';
+const GIT_PATH = './git-homebrew';
+// const GIT_URL = `ssh://git@github.com/CleverCloud/${GIT_PROJECT}.git`;
+const GIT_URL = `git@github.com:hsablonniere/test.git`;
+
+const [version] = process.argv.slice(2);
+if (version == null) {
+  throw new Error('Missing version');
+}
+
+const archivePath = getAssetPath('archive', version, 'build', 'macos');
+const sha256 = getSha256(archivePath);
+
+console.log(highlight`=> Cloning homebrew repository ${GIT_URL} to ${GIT_PATH}`);
+await simpleGit().clone(GIT_URL, GIT_PATH);
+
+console.log(highlight`=> Applying templates to ${GIT_PATH}`);
+await applyTemplates(GIT_PATH, TEMPLATES_PATH, {
+  description: pkg.description,
+  gitProject: GIT_PROJECT,
+  sha256,
+  url: pkg.homepage,
+  version,
+});
+
+await commitAndPush(GIT_PATH, GIT_URL, pkg.author, version);
